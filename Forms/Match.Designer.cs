32cc9e31a59de09b3e5d46bfc4f30c217ab35f29
DanielAssuncaoDeveloper <danielassuncao.developer@gmail.com>
2025-03-09 14:06:31 -0300

    :construction: Adicionando form da partida.

---
+++ Forms/Match.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace AutoSystem_KingMe.Forms
+{
+    partial class Match
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Match
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1242, 640);
+            Name = "Match";
+            Text = "Match";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Forms/Match.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoSystem_KingMe.Forms
+{
+    public partial class Match : Form
+    {
+        public Match()
+        {
+            InitializeComponent();
+        }
+    }
+}
